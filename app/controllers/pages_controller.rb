1413ace83c7caa071f061d2a87b13b7ef4bc199c
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-01-08 19:14:01 +0100

    added policy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,4 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :privacy_policy, :terms_and_conditions]
 
-  def home
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
   get 'set_status', to: 'pages#set_status'
+  get 'privacy_policy', to: 'pages#privacy_policy'
+  get 'terms_and_conditions', to: 'pages#terms_and_conditions'
 end
