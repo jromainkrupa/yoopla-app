a580663f48c4ad136978ddf81911b45241b79819
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-01-20 16:52:19 +0100

    begin the algo

--- app/models/program.rb
+++ app/models/program.rb
@@ -5,4 +5,15 @@ class Program < ApplicationRecord
   def number_of_days
     ((self.end-self.start)/86400 + 1).to_i
   end
+
+  def cigarette_allowed_for(date)
+    program_day = get_program_day(date)
+    (-init_smoke.fdiv(number_of_days) * (program_day) + init_smoke).round
+  end
+
+  private
+
+  def get_program_day(date)
+    (date - self.start).to_i
+  end
 end
