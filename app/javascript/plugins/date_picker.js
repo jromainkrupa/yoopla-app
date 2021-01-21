863ed17b0da5b7a43f6583bebf555079c19f3181
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-01-21 14:06:24 +0100

    improved calendar picker logic can't pick a date before start

--- app/javascript/plugins/date_picker.js
+++ app/javascript/plugins/date_picker.js
@@ -2,22 +2,27 @@ import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css"
 
 const datePicker = () => {
-  const startDate          = document.getElementById("range_start");
-
-  let today = new Date()
   const programForm = document.querySelector('#program-form-div');
 
    if (programForm) {
-    // const programs = JSON.parse(programForm.dataset.programs);
-    flatpickr("#range_start", {
-      altInput: true,
-      minDate: today.fp_incr(1)
-    });
+      const startDate   = document.getElementById("range_start");
+      const today       = new Date()
+      const rangeEndDiv = document.querySelector('#range-end-div')
 
-    flatpickr("#range_end", {
-      altInput: true,
-      minDate: new Date().fp_incr(15),
-      maxDate: new Date().fp_incr(180)
+      // fpStart est l'instance du range_start
+      const fpStart = flatpickr("#range_start", {
+        altInput: true,
+        minDate: today.fp_incr(1), // user can't pick the before tomorow
+      });
+
+    fpStart.config.onChange.push(function(selectedDates) {
+
+      rangeEndDiv.classList.remove('hidden')
+      flatpickr("#range_end", {
+        altInput: true,
+        minDate: selectedDates[0].fp_incr(15),
+        maxDate: new Date().fp_incr(180)
+      })
     })
   }
 }
