a332fc33e21553eac4b477c7021a4ccc2a272cb6
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-02-04 12:38:11 +0100

    signin user scenario valid inputs done

--- spec/features/user_can_sign_in_spec.rb
+++ spec/features/user_can_sign_in_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
 feature 'Sign_in User' do
-  before do
 
-  end
+  scenario 'with valid inputs' do
+    user = create(:user)
 
-  scenario 'valid inputs' do
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_on 'commit'
 
+    expect(page).to have_content('mentor')
+    expect(user.status).to eq(nil)
   end
 end
