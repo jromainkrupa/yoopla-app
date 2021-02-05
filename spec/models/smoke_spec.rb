6289ec687684a6e57a774a9f6534bd7999a3239c
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-02-05 15:08:08 +0100

    today scope test

--- spec/models/smoke_spec.rb
+++ spec/models/smoke_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe Smoke, type: :model do
+
   context 'Associations' do
     it 'increment smokes_count when persisted' do
       user = create(:user)
@@ -11,7 +12,8 @@ describe Smoke, type: :model do
       expect(user.smokes_count).to eq(1)
     end
   end
-  context 'validations' do
+
+  context 'Validations' do
     it 'persist a Smoke with valid inputs' do
       user = create(:user)
       Smoke.create(user: user)
@@ -22,6 +24,30 @@ describe Smoke, type: :model do
       smoke = Smoke.new(user: nil)
       smoke.valid?
       expect(smoke.errors[:user]).to include('must exist')
+    end
+  end
+
+  context 'today scope' do
+    it 'return all the cigarettes smoked today for one user' do
+      user = create(:user)
+      5.times do
+       Smoke.create(user: user)
+      end
+      3.times do
+        Smoke.create(user:user, created_at: Time.zone.now + 1.day)
+      end
+      expect(user.smokes.today.count).to eq(5)
+      expect(user.smokes_count).to eq(8)
+    end
+
+    it 'returns the number total of cigarettes smoked by all users ' do
+      user_one = create(:user)
+      user_two = create(:user, email:'another@another.com')
+      5.times do
+       Smoke.create(user: user_one)
+       Smoke.create(user: user_two)
+      end
+      expect(Smoke.today.count).to eq(10)
     end
   end
 
